5cab0069c4774d6e5ca7124df7f9ab14ea302d59
PPiovan <36052613+PPiovan@users.noreply.github.com>
2022-10-11 19:03:18 -0300

    Add files via upload

---
+++ Head-mickey.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Head-mickey) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define head-mickey (place-image(ellipse 7 10 "solid""black") 40 45
+                                (place-image(ellipse 7 10 "solid""black") 60 45
+                                (place-image(ellipse 10 20 "solid""white") 40 40
+                               (place-image(ellipse 10 20 "solid""white") 60 40
+                                (place-image(ellipse 20 10 "solid""black") 50 55
+                                 (place-image(ellipse 15 5 "solid""Salmon") 50 70
+                                (place-image(ellipse 20 10 "solid""Dark Salmon") 50 70
+                                 
+
+
+                                           
+                               (place-image(circle 30 "solid""Peach Puff")50 50
+                               (place-image(circle 15 "solid""black")30 20
+                                (place-image(circle 15 "solid""black")70 20
+
+                               (square 100 "solid""white"))))))))))))
+
+head-mickey
