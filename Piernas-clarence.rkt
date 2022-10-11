bc54644238a1772fc821060987fa4a8e706a65ae
PPiovan <36052613+PPiovan@users.noreply.github.com>
2022-10-11 18:50:39 -0300

    Add files via upload

---
+++ Piernas-clarence.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Piernas-clarence) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define piernas-clarence(place-image(rotate 45(ellipse 40 20 "solid" "Dark Purple"))30 70
+                                    (place-image(rotate 45(ellipse 40 20 "solid" "Dark Purple"))70 70
+                                    (place-image(rotate 90(ellipse 60 20 "solid" "Medium Pink"))35 40
+                        
+                                    (place-image(rotate 90(ellipse 60 20 "solid" "Medium Pink"))75 40
+                        
+
+
+                                    (square 100 "solid""black"))))))
+
+
+piernas-clarence
