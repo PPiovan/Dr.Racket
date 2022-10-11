f18654a7b2f6686f7e21a8d533085ea3b036cc96
PPiovan <36052613+PPiovan@users.noreply.github.com>
2022-10-11 20:01:12 -0300

    Add files via upload

---
+++ Piernas chicas superpoderosas.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Piernas chicas superpoderosas|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define leg-chicas (place-image(ellipse 15 10"solid""white")30 70
+                               (place-image(ellipse 15 10"solid""white")70 70
+
+
+                               (place-image(ellipse 25 30"solid""black")30 75
+                               (place-image(ellipse 25 30"solid""black")70 75
+
+
+                               (place-image(ellipse 30 80"solid""light grey")30 50
+                              (place-image(ellipse 30 80"solid""light grey")70 50
+
+                              (square 100 "solid""white"))))))))
+
+
+leg-chicas
